dd22a5341d39e0df573929f710039f66b7ac7cc6
Snape 479 <Severus_Snape93@protonmail.com>
2021-07-06 12:16:27 -0700

    end to end of txs

---
+++ impls/tests/chain_test_helper.rs
@@ -0,0 +1,313 @@
+// Copyright 2020 The Grin Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use bmw_wallet_util::grin_chain::types::NoopAdapter;
+use bmw_wallet_util::grin_chain::types::Options;
+use bmw_wallet_util::grin_chain::Chain;
+use bmw_wallet_util::grin_core::address::Address;
+use bmw_wallet_util::grin_core::core::hash::Hashed;
+use bmw_wallet_util::grin_core::core::pmmr;
+use bmw_wallet_util::grin_core::core::verifier_cache::LruVerifierCache;
+use bmw_wallet_util::grin_core::core::Block;
+use bmw_wallet_util::grin_core::core::BlockHeader;
+use bmw_wallet_util::grin_core::core::Transaction;
+use bmw_wallet_util::grin_core::genesis;
+use bmw_wallet_util::grin_core::global::ChainTypes;
+use bmw_wallet_util::grin_core::libtx;
+use bmw_wallet_util::grin_core::libtx::proof::PaymentId;
+use bmw_wallet_util::grin_core::libtx::proof::ProofBuild;
+use bmw_wallet_util::grin_core::libtx::ProofBuilder;
+use bmw_wallet_util::grin_core::pow::Difficulty;
+use bmw_wallet_util::grin_core::{consensus, global, pow};
+use bmw_wallet_util::grin_keychain as keychain;
+use bmw_wallet_util::grin_keychain::keychain::SecretKey;
+use bmw_wallet_util::grin_keychain::Keychain;
+use bmw_wallet_util::grin_util::RwLock;
+use chrono::Duration;
+use rand::thread_rng;
+use std::fs;
+use std::sync::Arc;
+
+#[allow(dead_code)]
+pub fn build_block_from_prev<K>(
+	header: &BlockHeader,
+	chain: &Chain,
+	keychain: &K,
+	txs: Vec<Transaction>,
+	recipient_address: Address,
+	private_nonce: SecretKey,
+	set_roots: bool,
+) -> Block
+where
+	K: Keychain,
+{
+	build_block_impl(
+		header,
+		chain,
+		keychain,
+		txs,
+		recipient_address,
+		private_nonce,
+		set_roots,
+	)
+}
+
+#[allow(dead_code)]
+pub fn build_block<K>(
+	chain: &Chain,
+	keychain: &K,
+	txs: Vec<Transaction>,
+	recipient_address: Address,
+	private_nonce: SecretKey,
+	set_roots: bool,
+) -> Block
+where
+	K: Keychain,
+{
+	build_block_impl(
+		&chain.head_header().unwrap(),
+		chain,
+		keychain,
+		txs,
+		recipient_address,
+		private_nonce,
+		set_roots,
+	)
+}
+
+fn build_block_impl<K>(
+	prev: &BlockHeader,
+	chain: &Chain,
+	keychain: &K,
+	txs: Vec<Transaction>,
+	recipient_address: Address,
+	private_nonce: SecretKey,
+	set_roots: bool,
+) -> Block
+where
+	K: Keychain,
+{
+	let next_header_info = consensus::next_difficulty(1, chain.difficulty_iter().unwrap());
+
+	let mut block = new_block_with_private_nonce(
+		&txs,
+		keychain,
+		&ProofBuilder::new(keychain),
+		&prev,
+		recipient_address,
+		private_nonce,
+	);
+
+	block.header.timestamp = prev.timestamp + Duration::seconds(60);
+	block.header.pow.secondary_scaling = next_header_info.secondary_scaling;
+
+	if set_roots {
+		chain.set_txhashset_roots(&mut block).unwrap();
+	} else {
+		chain.set_prev_root_only(&mut block.header).unwrap();
+		block.header.output_mmr_size = pmmr::insertion_to_pmmr_index(prev.output_mmr_count() + 1);
+		block.header.kernel_mmr_size = pmmr::insertion_to_pmmr_index(prev.kernel_mmr_count() + 1);
+	}
+
+	let edge_bits = global::min_edge_bits();
+	block.header.pow.proof.edge_bits = edge_bits;
+	pow::pow_size(
+		&mut block.header,
+		next_header_info.difficulty,
+		global::proofsize(),
+		edge_bits,
+	)
+	.unwrap();
+
+	block
+}
+
+#[allow(dead_code)]
+pub fn new_block<K, B>(
+	txs: &[Transaction],
+	keychain: &K,
+	builder: &B,
+	previous_header: &BlockHeader,
+	recipient_addr: Address,
+) -> Block
+where
+	K: Keychain,
+	B: ProofBuild,
+{
+	let (private_nonce, _pub_nonce) = keychain.secp().generate_keypair(&mut thread_rng()).unwrap();
+	new_block_with_private_nonce(
+		txs,
+		keychain,
+		builder,
+		previous_header,
+		recipient_addr,
+		private_nonce,
+	)
+}
+
+#[allow(dead_code)]
+pub fn new_block_with_private_nonce<K, B>(
+	txs: &[Transaction],
+	keychain: &K,
+	builder: &B,
+	previous_header: &BlockHeader,
+	recipient_addr: Address,
+	private_nonce: SecretKey,
+) -> Block
+where
+	K: Keychain,
+	B: ProofBuild,
+{
+	let fees = txs
+		.iter()
+		.map(|tx| tx.fee(previous_header.height + 1))
+		.sum();
+
+	let payment_id = PaymentId::new();
+
+	let reward_output = libtx::reward::nit_output(
+		keychain,
+		builder,
+		private_nonce,
+		recipient_addr.clone(),
+		payment_id,
+		fees,
+		false,
+		1,
+	)
+	.unwrap();
+
+	Block::new(
+		&previous_header,
+		txs,
+		Difficulty::min_dma(),
+		reward_output,
+		None,
+	)
+	.unwrap()
+}
+
+#[allow(dead_code)]
+pub fn clean_output_dir(dir_name: &str) {
+	let _ = fs::remove_dir_all(dir_name);
+}
+
+pub fn init_chain(dir_name: &str, genesis: Block) -> Chain {
+	let verifier_cache = Arc::new(RwLock::new(LruVerifierCache::new()));
+	Chain::init(
+		dir_name.to_string(),
+		Arc::new(NoopAdapter {}),
+		genesis,
+		pow::verify_size,
+		verifier_cache,
+		false,
+		None,
+	)
+	.unwrap()
+}
+
+/// Build genesis block with reward (non-empty, like we have in mainnet).
+pub fn genesis_block<K>(_keychain: &K) -> Block
+where
+	K: Keychain,
+{
+	genesis::genesis_dev().without_reward()
+}
+
+/// Mine a chain of specified length to assist with automated tests.
+/// Probably a good idea to call clean_output_dir at the beginning and end of each test.
+#[allow(dead_code)]
+pub fn mine_chain(dir_name: &str, chain_length: u64) -> Chain {
+	global::set_local_chain_type(ChainTypes::AutomatedTesting);
+	let keychain = keychain::ExtKeychain::from_random_seed(false).unwrap();
+	let genesis = genesis_block(&keychain);
+	let mut chain = init_chain(dir_name, genesis.clone());
+	mine_some_on_top(&mut chain, chain_length, &keychain);
+	chain
+}
+
+#[allow(dead_code)]
+fn mine_some_on_top<K>(chain: &mut Chain, chain_length: u64, keychain: &K)
+where
+	K: Keychain,
+{
+	for n in 1..chain_length {
+		let prev = chain.head_header().unwrap();
+		let next_header_info =
+			consensus::next_difficulty(prev.height + 1, chain.difficulty_iter().unwrap());
+		let (private_nonce, _pub_nonce) =
+			keychain.secp().generate_keypair(&mut thread_rng()).unwrap();
+		let (_pri_view, pub_view) = keychain.secp().generate_keypair(&mut thread_rng()).unwrap();
+		let recipient_addr =
+			Address::from_one_pubkey(&pub_view, global::ChainTypes::AutomatedTesting);
+		let reward = libtx::reward::nit_output(
+			keychain,
+			&libtx::ProofBuilder::new(keychain),
+			private_nonce,
+			recipient_addr.clone(),
+			PaymentId::new(),
+			0,
+			false,
+			prev.height + 1,
+		)
+		.unwrap();
+		let mut b = bmw_wallet_util::grin_core::core::Block::new(
+			&prev,
+			&[],
+			next_header_info.difficulty,
+			reward,
+			None,
+		)
+		.unwrap();
+		b.header.timestamp = prev.timestamp + Duration::seconds(60);
+		b.header.pow.secondary_scaling = next_header_info.secondary_scaling;
+
+		chain.set_txhashset_roots(&mut b).unwrap();
+
+		let edge_bits = global::min_edge_bits();
+		b.header.pow.proof.edge_bits = edge_bits;
+		pow::pow_size(
+			&mut b.header,
+			next_header_info.difficulty,
+			global::proofsize(),
+			edge_bits,
+		)
+		.unwrap();
+
+		let bhash = b.hash();
+		chain.process_block(b, Options::MINE).unwrap();
+
+		// checking our new head
+		let head = chain.head().unwrap();
+		assert_eq!(head.height, n);
+		assert_eq!(head.last_block_h, bhash);
+
+		// now check the block_header of the head
+		let header = chain.head_header().unwrap();
+		assert_eq!(header.height, n);
+		assert_eq!(header.hash(), bhash);
+
+		// now check the block itself
+		let block = chain.get_block(&header.hash()).unwrap();
+		assert_eq!(block.header.height, n);
+		assert_eq!(block.hash(), bhash);
+		assert_eq!(block.outputs().len(), 1);
+
+		// now check the block height index
+		let header_by_height = chain.get_header_by_height(n).unwrap();
+		assert_eq!(header_by_height.hash(), bhash);
+
+		chain.validate(false).unwrap();
+	}
+}
--- impls/tests/wallet.rs
+++ impls/tests/wallet.rs
@@ -14,14 +14,129 @@
 
 //! Wallet tests
 
+#[macro_use]
+extern crate log;
+
 use bmw_wallet_config::config::AccountArgs;
 use bmw_wallet_config::config::InitArgs;
+use bmw_wallet_config::config::TxsArgs;
 use bmw_wallet_config::config::WalletConfig;
 use bmw_wallet_impls::wallet::Wallet;
+use bmw_wallet_impls::HTTPNodeClient;
 use bmw_wallet_libwallet::WalletInst;
 use bmw_wallet_util::grin_core::global;
 use bmw_wallet_util::grin_core::global::ChainTypes;
+use bmw_wallet_util::grin_p2p::msg::PeerAddrs;
+use bmw_wallet_util::grin_p2p::types::PeerAddr;
+use bmw_wallet_util::grin_p2p::Seeding;
+use bmw_wallet_util::grin_servers as servers;
+use bmw_wallet_util::grin_servers::ServerConfig;
+use bmw_wallet_util::grin_util::logger::LogEntry;
+use std::net::SocketAddr;
+use std::net::{IpAddr, Ipv4Addr};
 use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::thread;
+use std::time::Duration;
+mod chain_test_helper;
+
+//use self::chain_test_helper::{build_block, init_chain, mine_chain, new_block};
+
+fn build_server_config(api_str: &str, port: u16, tor_port: u16, data_dir: &str) -> ServerConfig {
+	let mut config = ServerConfig::default();
+	config.db_root = data_dir.to_string();
+	config.api_http_addr = api_str.to_string();
+	config.p2p_config.tor_port = tor_port;
+	config.skip_sync_wait = Some(true);
+	config.p2p_config.port = port;
+	config.p2p_config.seeding_type = Seeding::List;
+	config.p2p_config.seeds = Some(PeerAddrs {
+		peers: vec![
+			PeerAddr::Ip(SocketAddr::new(
+				IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+				23494,
+			)),
+			PeerAddr::Ip(SocketAddr::new(
+				IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+				23394,
+			)),
+			PeerAddr::Ip(SocketAddr::new(
+				IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+				23294,
+			)),
+			PeerAddr::Ip(SocketAddr::new(
+				IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+				23194,
+			)),
+		],
+	});
+	config
+}
+
+fn spawn_server(config: ServerConfig) {
+	let (_logs_tx, logs_rx) = mpsc::sync_channel::<LogEntry>(200);
+	thread::spawn(|| {
+		servers::Server::start(
+			config,
+			Some(logs_rx),
+			|serv: servers::Server, _: Option<mpsc::Receiver<LogEntry>>| {
+				global::set_local_chain_type(global::ChainTypes::UserTesting);
+				let running = Arc::new(AtomicBool::new(true));
+				let r = running.clone();
+				ctrlc::set_handler(move || {
+					r.store(false, Ordering::SeqCst);
+				})
+				.expect("Error setting handler for both SIGINT (Ctrl+C) and SIGTERM (kill)");
+				while running.load(Ordering::SeqCst) {
+					thread::sleep(Duration::from_secs(1));
+				}
+				serv.stop();
+			},
+		)
+		.unwrap();
+	});
+}
+
+fn start_test_server(_data_dir: &str) {
+	global::init_global_chain_type(global::ChainTypes::UserTesting);
+
+	// start server 1
+	let config = build_server_config("127.0.0.1:23493", 23494, 23497, "tests/resources/1");
+	spawn_server(config);
+
+	// start server 2
+	//let config = build_server_config("127.0.0.1:23393", 23394, 23397, "2");
+	//spawn_server(config);
+
+	// start server 3
+	//let config = build_server_config("127.0.0.1:23293", 23294, 23297, "3");
+	//spawn_server(config);
+
+	// start server 4
+	//let config = build_server_config("127.0.0.1:23193", 23194, 23197, "4");
+	//spawn_server(config);
+
+	std::thread::sleep(std::time::Duration::from_millis(10 * 1000));
+
+	let mut count = 0;
+	loop {
+		count += 1;
+		let client = HTTPNodeClient::new("http://127.0.0.1:23493", None);
+		let res = client.chain_height();
+		if count == 100 {
+			break;
+		}
+		if res.is_err() {
+			debug!("can't connect");
+			continue;
+		}
+		debug!("height: {}", res.unwrap().0);
+		break;
+	}
+}
 
 pub fn clean_output_dir(test_dir: &str) {
 	let _ = std::fs::remove_dir_all(test_dir);
@@ -33,8 +148,10 @@ fn get_wallet_instance() -> Wallet {
 
 fn build_config(
 	dir: &str,
+	node: &str,
 	init_args: Option<InitArgs>,
 	account_args: Option<AccountArgs>,
+	txs_args: Option<TxsArgs>,
 ) -> WalletConfig {
 	let mut path = PathBuf::new();
 	path.push(dir);
@@ -45,7 +162,7 @@ fn build_config(
 		create_path: false,
 		sub_command: "account".to_string(),
 		account: "default".to_string(),
-		node: "".to_string(),
+		node: format!("http://{}", node.to_string()),
 		node_api_secret: None,
 		init_args,
 		outputs_args: None,
@@ -54,7 +171,7 @@ fn build_config(
 		claim_args: None,
 		cancel_args: None,
 		account_args,
-		txs_args: None,
+		txs_args,
 		pass: None,
 	};
 	config
@@ -62,6 +179,7 @@ fn build_config(
 
 #[test]
 fn test_init() {
+	bmw_wallet_util::grin_util::init_test_logger();
 	let test_dir = ".bmw_wallet_init";
 	clean_output_dir(test_dir);
 	global::set_local_chain_type(global::ChainTypes::UserTesting);
@@ -69,11 +187,13 @@ fn test_init() {
 	let mut wallet = get_wallet_instance();
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		Some(InitArgs {
 			here: true,
 			recover: false,
 			recover_phrase: None,
 		}),
+		None,
 		None,
 	);
 	let res = wallet.init(&config, "").unwrap();
@@ -85,6 +205,7 @@ fn test_init() {
 
 #[test]
 fn test_account() {
+	bmw_wallet_util::grin_util::init_test_logger();
 	let test_dir = ".bmw_wallet_account";
 	clean_output_dir(test_dir);
 	global::set_local_chain_type(global::ChainTypes::UserTesting);
@@ -92,17 +213,25 @@ fn test_account() {
 	let mut wallet = get_wallet_instance();
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		Some(InitArgs {
 			here: true,
 			recover: false,
 			recover_phrase: None,
 		}),
 		None,
+		None,
 	);
 	let init_resp = wallet.init(&config, "").unwrap();
 	assert_eq!(init_resp.get_mnemonic().is_ok(), true);
 
-	let config = build_config(test_dir, None, Some(AccountArgs { create: None }));
+	let config = build_config(
+		test_dir,
+		"127.0.0.1:23493",
+		None,
+		Some(AccountArgs { create: None }),
+		None,
+	);
 	let account_resp = wallet.account(&config, "").unwrap();
 	// no accounts created
 	assert_eq!(account_resp.created().unwrap().is_some(), false);
@@ -112,10 +241,12 @@ fn test_account() {
 	// create an account
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		None,
 		Some(AccountArgs {
 			create: Some("test".to_string()),
 		}),
+		None,
 	);
 
 	let account_resp = wallet.account(&config, "").unwrap();
@@ -134,6 +265,7 @@ fn test_account() {
 
 #[test]
 fn test_address() {
+	bmw_wallet_util::grin_util::init_test_logger();
 	let test_dir = ".bmw_wallet_address";
 	clean_output_dir(test_dir);
 	global::set_local_chain_type(global::ChainTypes::UserTesting);
@@ -141,23 +273,28 @@ fn test_address() {
 	let mut wallet = get_wallet_instance();
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		Some(InitArgs {
 			here: true,
 			recover: false,
 			recover_phrase: None,
 		}),
 		None,
+		None,
 	);
 	let init_resp = wallet.init(&config, "").unwrap();
 	assert_eq!(init_resp.get_mnemonic().is_ok(), true);
 
-	let config = build_config(test_dir, None, None);
+	let config = build_config(test_dir, "127.0.0.1:23493", None, None, None);
 	let address_response = wallet.address(&config, "").unwrap();
 	assert_eq!(address_response.get_address().is_ok(), true);
+
+	clean_output_dir(test_dir);
 }
 
 #[test]
 fn test_info() {
+	bmw_wallet_util::grin_util::init_test_logger();
 	let test_dir = ".bmw_wallet_info";
 	clean_output_dir(test_dir);
 	global::set_local_chain_type(global::ChainTypes::UserTesting);
@@ -165,45 +302,65 @@ fn test_info() {
 	let mut wallet = get_wallet_instance();
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		Some(InitArgs {
 			here: true,
 			recover: false,
 			recover_phrase: None,
 		}),
 		None,
+		None,
 	);
 	let init_resp = wallet.init(&config, "").unwrap();
 	assert_eq!(init_resp.get_mnemonic().is_ok(), true);
 
-	let config = build_config(test_dir, None, None);
+	let config = build_config(test_dir, "127.0.0.1:23493", None, None, None);
 	let info_response = wallet.info(&config, "").unwrap();
 	assert_eq!(info_response.get_output_count().unwrap(), 0);
 	assert_eq!(info_response.get_height().unwrap(), 0);
 	assert_eq!(info_response.get_balance().unwrap(), 0.0);
 	assert_eq!(info_response.get_spendable().unwrap(), 0.0);
+
+	clean_output_dir(test_dir);
 }
 
 #[test]
 fn test_txs() {
+	bmw_wallet_util::grin_util::init_test_logger();
 	let test_dir = ".bmw_wallet_txs";
 	clean_output_dir(test_dir);
 	global::set_local_chain_type(global::ChainTypes::UserTesting);
+	// start the server
+	start_test_server(test_dir);
+	//std::thread::sleep(std::time::Duration::from_millis(10 * 1000));
 
 	let mut wallet = get_wallet_instance();
 	let config = build_config(
 		test_dir,
+		"127.0.0.1:23493",
 		Some(InitArgs {
 			here: true,
 			recover: false,
 			recover_phrase: None,
 		}),
 		None,
+		None,
 	);
 	let init_resp = wallet.init(&config, "").unwrap();
 	assert_eq!(init_resp.get_mnemonic().is_ok(), true);
 
-	let config = build_config(test_dir, None, None);
+	let config = build_config(
+		test_dir,
+		"127.0.0.1:23493",
+		None,
+		None,
+		Some(TxsArgs {
+			payment_id: None,
+			tx_id: None,
+		}),
+	);
 	let txs_response = wallet.txs(&config, "");
-	// TODO: fix, shouldn't be an error
-	assert_eq!(txs_response.is_err(), true);
+	assert_eq!(txs_response.is_err(), false);
+
+	clean_output_dir(test_dir);
 }
